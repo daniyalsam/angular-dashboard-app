150ba6aaef66723805d381cb8cfe38ebee4177c1
daniyalsam <daniyalbtech@gmail.com>
2016-11-25 15:41:32 +0530

    Update random.js
    for my knowledge

--- app/scripts/widgets/random/random.js
+++ app/scripts/widgets/random/random.js
@@ -8,10 +8,10 @@ angular.module('ui.dashboard.widgets')
       templateUrl: 'scripts/widgets/random/random.html',
       link: function postLink(scope) {
         function update() {
-          scope.number = Math.floor(Math.random() * 100);
+          scope.number = Math.floor(Math.random() * 10);
         }
 
-        var promise = $interval(update, 500);
+        var promise = $interval(update, 50);
 
         scope.$on('$destroy', function () {
           $interval.cancel(promise);
